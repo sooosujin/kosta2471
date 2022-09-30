7a86b52874bbf613fc8021b7d08703c6e9fab973
jiyoung yu <jyjyu1204@gmail.com>
2022-09-30 12:35:54 +0900

    branch mission start

--- GitProject_1/src/kosta1/Member1.java
+++ GitProject_1/src/kosta1/Member1.java
@@ -6,5 +6,29 @@ public class Member1 {
 	
 	private String name;
 	private int phoneNum;
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPhoneNum() {
+		return phoneNum;
+	}
+
+	public void setPhoneNum(int phoneNum) {
+		this.phoneNum = phoneNum;
+	}
+
+	public Member1(String name, int phoneNum) {
+		super();
+		this.name = name;
+		this.phoneNum = phoneNum;
+	}
+	
+	
 
 }
